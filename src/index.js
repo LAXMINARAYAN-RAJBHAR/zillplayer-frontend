ac189cbc8a38dc820d20fa5f96c5d7377b53775a
Laxminarayan Rajbhar <rajbhar.laxminarayan01@gmail.com>
2026-05-07 00:00:49 +0530

    fix: router config for both deployments

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+
+const isGitHubPages = window.location.hostname.includes("github.io");
+const Router = isGitHubPages ? HashRouter : BrowserRouter;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/rollamroll-frontend">
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
 );
 
