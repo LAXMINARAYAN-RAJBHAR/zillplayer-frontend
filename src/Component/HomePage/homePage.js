e99a0dca67793adfe03fda0bec8de5a397f67a1c
Laxminarayan Rajbhar <rajbhar.laxminarayan01@gmail.com>
2026-05-18 17:56:44 +0530

    update: New changes

--- src/Component/HomePage/homePage.js
+++ src/Component/HomePage/homePage.js
@@ -1529,7 +1529,7 @@ const WatchPage = ({
         <style>{`
   .hp-watch-root {
     position: fixed;
-    top: 60px;        /* ← below navbar */
+    top: 55px;        /* ← below navbar */
     left: 0; right: 0; bottom: 0;
     z-index: 999;     /* ← lower than navbar */
     background: #0f0f0f;
